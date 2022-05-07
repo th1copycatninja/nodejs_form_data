7bf451254966c57819394658e25c094d4d6df348
govind1530 <mm2406402@gmail.com>
2022-05-07 09:34:38 +0530

    handle multiple and single image

--- index.js
+++ index.js
@@ -1,40 +1,62 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const fileUpload = require('express-fileupload');
-const cloudinary = require('cloudinary').v2;
+const fileUpload = require("express-fileupload");
+const cloudinary = require("cloudinary").v2;
 cloudinary.config({
-    cloud_name:"formandmedianodejs",
-    api_key:"264551678479288",
-    api_secret:"S9tEYDYOHyWCSwVa8wzUwS_uSIE"
-})
+  cloud_name: "formandmedianodejs",
+  api_key: "264551678479288",
+  api_secret: "S9tEYDYOHyWCSwVa8wzUwS_uSIE",
+});
 app.use(express.json());
-app.set("view engine","ejs");
-app.use(express.urlencoded({extended:true}));
-app.use(fileUpload({
-    useTempFiles:true,
-    tempFileDir:""
-}))
-app.get("/getquery",(req,res)=>{
-    console.log(req.query);
-    return res.send(req.query)
-})
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "",
+  })
+);
+app.get("/getquery", (req, res) => {
+  console.log(req.query);
+  return res.send(req.query);
+});
 
-app.post("/postquery",async(req,res)=>{
-    console.log(req.body);
-    console.log(req.files)
-    const imageFile = req.files.samplefile
-    const result =await cloudinary.uploader.upload(imageFile.tempFilePath,{
-        folder:"users"
-    })
-    console.log("image result ---",result);
-    return res.send({data:{users:req.body,image:result}});
-})
+app.post("/postquery", async (req, res) => {
+  console.log(req.body);
+  console.log(req.files);
 
-app.get("/getejs",(req,res)=>{
-    res.render("getejs")
-})
+  // multi image upload
 
-app.get("/postejs",(req,res)=>{
-    res.render("postejs")
-})
-app.listen("4000",()=>console.log("running is running on port 4000"));
+  let imageArray = [];
+  if (req?.files?.samplefile) {
+    for (let index = 0; index < req.files.samplefile.length; index++) {
+      const result = await cloudinary.uploader.upload(
+        req.files.samplefile[index].tempFilePath,
+        {
+          folder: "users",
+        }
+      );
+      imageArray.push({
+        public_key: result.public_id,
+        http_url: result.url,
+        secure_url: result.secure_url,
+      });
+    }
+  }
+  //single image upload
+  const imageFile = req.files.samplefile
+  const result =await cloudinary.uploader.upload(imageFile.tempFilePath,{
+      folder:"users"
+  })
+  console.log("image result ---", imageArray);
+  return res.send({ data: { users: req.body, image:result?result: imageArray } });
+});
+
+app.get("/getejs", (req, res) => {
+  res.render("getejs");
+});
+
+app.get("/postejs", (req, res) => {
+  res.render("postejs");
+});
+app.listen("4000", () => console.log("running is running on port 4000"));
